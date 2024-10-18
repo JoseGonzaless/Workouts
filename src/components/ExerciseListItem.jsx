4b0ebfabc1c35d4531211b68fc178f86b178b55c
Jose Gonzales <118575003+JoseGonzaless@users.noreply.github.com>
2024-10-18 13:29:09 -0500

    Setup expo router

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -14,12 +14,22 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       padding: 10,
       borderRadius: 10,
-      gap: 5
+      gap: 5,
+      marginHorizontal: 2,
+
+      //shadow
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 1,
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 1.41,
+      elevation: 2,
     },
     exerciseName: {
       fontSize: 20, 
       fontweight: '500',
-  
     },
     exerciseSubtitle: {
       color: 'dimgray'
